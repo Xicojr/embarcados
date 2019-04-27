34209e288042c074b0760a6d909998404af4a511
Francisco das Chagas Peres Júnior <38120807+Xicojr@users.noreply.github.com>
2019-04-27 10:21:36 -0300

    Add files via upload

--- Display7seg.X/main.c
+++ Display7seg.X/main.c
@@ -1,13 +1,28 @@
-/*
- * File:   main.c
- * Author: franc
- *
- * Created on 27 de Abril de 2019, 09:40
- */
-
+#pragma config FOSC = INTOSCCLK // Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital input, MCLR internally tied to VDD)
+#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB4/PGM pin has digital I/O function, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
 #include <xc.h>
+#define _XTAL_FREQ 4000000 
 
 void main(void) {
-    return;
+    int  digito[4] = {63, 6, 91, 79};
+    int contador = 0;
+    TRISA = 0;
+    PORTA = 0;
+    
+    while(1){
+        PORTA = digito[contador];
+        __delay_ms(1000);
+        
+        if(contador<4)
+            contador++;
+        else
+            contador == 0;
+    }
 }
