d567b630dd29f5b1dc2cde52b209c758195ff2bc
Francisco das Chagas Peres Júnior <38120807+Xicojr@users.noreply.github.com>
2019-04-27 10:13:41 -0300

    Add files via upload

---
+++ Display7seg.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: franc
+ *
+ * Created on 27 de Abril de 2019, 09:40
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ HellloPIC.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HellloPIC.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HellloPIC.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=helllopic.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/helllopic.x.tar
